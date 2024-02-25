66b8b823903e81b56d706914c19e82966069466f
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2024-02-25 00:18:14 +0000

    started on search

---
+++ src/utils/search.js
@@ -0,0 +1,9 @@
+const stores = require('stores')
+
+const searchStore = async (item, store) => {
+    // const searchLink = stores[store] + item
+    const searchLink = `https://priceable.co.uk/search?q=${item}&retailer=${store}`
+
+    
+}
+
