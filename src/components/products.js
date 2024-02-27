af3dfcc8d8dde51887a1f5ad60a9afc2da134066
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2024-02-27 11:31:56 +0000

    created product class

---
+++ src/components/products.js
@@ -0,0 +1,9 @@
+class Product {
+    constructor(name, picture, price, category, store) {
+        this.name = name, 
+        this.picture = picture, 
+        this.price = price,
+        this.category = category,
+        this.store - store,
+    }
+}
