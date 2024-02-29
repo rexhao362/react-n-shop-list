43e9cdecf358b75f10080810bfb74dec1710f6de
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2024-02-29 20:05:52 +0000

    setting up cheerio

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,15 +1,16 @@
-const cheerio = require('cheerio');
-const storeUrls = require('./stores')
+const cheerio = require('cheerio')
+const storeDetails = require('./stores/storeDetails')
 
 const searchStore = async (item, store) => {
-    const searchUrl = storeUrls[store] + item
+    const webData = storeDetails[store]
+    const searchUrl =  webData.url + item
 
     const res = await fetch(searchUrl)
     const htmlString = res.text()
     const $ = cheerio.load(htmlString)
-    
+    return $(webData.item).map((_, li) => ({
+        name: $(webData.name, li).text(),
+    }))
 }
 
-searchStore('chocolate', 'Tesco')
-    .then(res => console.log(res))
-
+searchStore('chocolate', 'Tesco').then(res => console.log(res))
